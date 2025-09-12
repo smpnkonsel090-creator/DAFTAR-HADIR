40e78422c19241512093903909ce35575d31ab4b
smpnkonsel090-creator <smpnkonsel090@gmail.com>
2025-09-12 15:52:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-// Firebase JS SDK v12
+// config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyCAg9Kb4I3dp-fcCIp5RzQKY8mJ3sD4hPU",
   authDomain: "absensi-smpn-20-konsel.firebaseapp.com",
-  databaseURL: "https://absensi-smpn-20-konsel-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "absensi-smpn-20-konsel",
-  storageBucket: "absensi-smpn-20-konsel.firebasestorage.app",
+  storageBucket: "absensi-smpn-20-konsel.appspot.com",
   messagingSenderId: "247838919432",
   appId: "1:247838919432:web:4505c7d499fb5d716dfabd",
   measurementId: "G-1TNJRBYFEV"
